0667523f7bd1c1ff267c0efe4aefd61f654ae077
Unknown <peerawit.sc@gmail.com>
2018-01-29 00:51:06 +0700

    add grade.csv and edit function show_data

--- calculate_gradeDB.py
+++ calculate_gradeDB.py
@@ -40,7 +40,6 @@ class CalulateGrade(object):
 		print("Table Created")
 
 	def insert_data(self):
-
 		rows = ''
 		for i in range(len(self.column[self.header[0]])):
 			rows += "('{}','{}','{}','{}','{}','{}','{}')".format(self.column[self.header[0]][i], 
@@ -50,7 +49,6 @@ class CalulateGrade(object):
 				rows += ','
 
 		insertData = "INSERT INTO grade VALUES"+rows
-
 		try:
 			self.myCursor.execute(insertData)
 			self.conn.commit()
@@ -60,31 +58,39 @@ class CalulateGrade(object):
 		self.conn.close()
 		print("Data Inserted")
 
-	def show_data(self):
-		showData = """SELECT * FROM grade """
-		a = self.myCursor.execute(showData)
+	def show_data(self, text):
+		showData = """SELECT * FROM grade where {}""".format(text)
+		num = self.myCursor.execute(showData)
 		data = self.myCursor.fetchall()
-		for i in range(a):
-			print(data[i])
+		print("Number:", num)
+		for i in range(num):
+			for j in range(len(data[i])):
+				print("{}:{}".format(self.header[j], data[i][j]), end=" ")
+			print()
 
 	def show_grade(self):
 		year, semester = list(set(self.column['Year'])), list(set(self.column['Semester']))
 		year.sort(), semester.sort()
+		total_credit = 0; total_score = 0
 		for y in range(len(year)):
  			for s in range(len(semester)):
  				showGrade = """SELECT Credit, Score 
  							FROM grade WHERE Year = {} AND Semester = {}""".format(year[y], semester[s])
  				num = self.myCursor.execute(showGrade)
  				if num != 0:
- 					sum_credit = 0
- 					sum_score = 0
+ 					sum_credit = 0; sum_score = 0
  					data = self.myCursor.fetchall()
- 					for i in range(len(data)):
+ 					for i in range(len(data)): 
  						sum_credit += data[i][0]
  						sum_score += data[i][1]*data[i][0]
- 					print("{}/{}: {:.2f}".format(year[y], semester[s], sum_score/sum_credit))
+ 					total_score += sum_score
+ 					total_credit += sum_credit
+ 					gpa = sum_score/sum_credit
+ 					gpax = total_score/total_credit
+ 					print("{}/{} => GPA: {:.2f} GPAX: {:.2f}".format(year[y], semester[s], gpa, gpax))
 
 grade = CalulateGrade("grade.csv")
 #grade.create_table()
 #grade.insert_data()
-grade.show_grade()
+#grade.show_grade()
+grade.show_data("Grade = 'A'")
